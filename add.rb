9846ef8f6896549ae3d154f1bc2beb4e0743d877
Walid Asmar <Walid.e.asmar@gmail.com>
2016-01-21 09:01:25 -0500

    example.rb

--- add.rb
+++ add.rb
@@ -1,5 +1,5 @@
-#Write a program that keeps asking the user for a number
-#until they type done. Puts the addition of the numbers.
+# Write a program that keeps asking the user for a number
+# until they type done. Puts the addition of the numbers.
 response = ''
 newnum = 0
 until response == 'done'
